9148e6c9773bf2ca9a1757926bec2a7b743a91ae
FrederikOlesen <Frederik.o@mailme.dk>
2015-02-26 15:26:10 +0100

    Possible to retrieve information from Java RestAPI now.

--- public/app/view1/view1.js
+++ public/app/view1/view1.js
@@ -4,10 +4,22 @@ angular.module('myAppRename.view1', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {
-    templateUrl: '/partials/partial1',
+    templateUrl: '/view1/students.html',
     controller: 'View1Ctrl'
   });
 }])
 
-.controller('View1Ctrl', function() {
-});
+.controller('View1Ctrl', ['$scope', '$http', function ($scope, $http) {
+      $http({
+        method: 'GET',
+        url: 'http://localhost:8080/data/'
+
+      }).
+          success(function (data) {
+            console.log("Data: " + data);
+            $scope.students = data;
+          }).
+          error(function (data) {
+            $scope.error = data;
+          });
+}]);
