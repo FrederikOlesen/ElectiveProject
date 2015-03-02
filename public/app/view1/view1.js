4822d4da9626a0f47f3460cf7d14e0da48a06fb7
unknown <fol@jetnb220.jet-time.local>
2015-03-02 12:46:16 +0100

    Started working on GUI presentation.

--- public/app/view1/view1.js
+++ public/app/view1/view1.js
@@ -2,24 +2,25 @@
 
 angular.module('myAppRename.view1', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
-    templateUrl: '/view1/students.html',
-    controller: 'View1Ctrl'
-  });
-}])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/view1', {
+            templateUrl: '/view1/students.html',
+            controller: 'View1Ctrl'
+        });
+    }])
 
-.controller('View1Ctrl', ['$scope', '$http', function ($scope, $http) {
-      $http({
-        method: 'GET',
-        url: 'http://localhost:8080/data/'
+    .controller('View1Ctrl', ['$scope', '$http', function ($scope, $http) {
+        $http({
+            method: 'GET',
+            url: 'http://localhost:8080/data'
 
-      }).
-          success(function (data) {
-            console.log("Data: " + data);
-            $scope.students = data;
-          }).
-          error(function (data) {
-            $scope.error = data;
-          });
-}]);
+        }).
+            success(function (data) {
+                var json = angular.toJson(data);
+                console.log("Data: " + data);
+                $scope.students = data;
+            }).
+            error(function (data) {
+                $scope.error = data;
+            });
+    }]);
--- public/app/view2/view2.js
+++ public/app/view2/view2.js
@@ -2,14 +2,32 @@
 
 angular.module('myAppRename.view2', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view2', {
-    templateUrl: 'app/view2/view2.html',
-    controller: 'View2Ctrl'
-  });
-}])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/view2', {
+            templateUrl: '/view2/addsubject.html',
+            controller: 'View2Ctrl'
+        });
+    }])
 
-.controller('View2Ctrl',['$scope','InfoFactory','InfoService', function($scope,InfoFactory,InfoService) {
-    $scope.infoFactory = InfoFactory.getInfo();
-    $scope.infoService = InfoService.getInfo();
-  }]);
+    .controller('View2Ctrl', ['$scope', '$http', function ($scope, $http) {
+        $scope.saveSubject = function () {
+            var data = {
+                title: $scope.subjecttitle,
+                description: $scope.description,
+                teachers: $scope.teacher
+            }
+
+            var json = angular.toJson(data);
+
+            console.log("JSON : " + json);
+
+            $http.post('http://localhost:8080/data/', json)
+               .success(function (data) {
+                    console.log("Data: " + data);
+                    $scope.students = data;
+                }).
+                error(function (data) {
+                    $scope.error = data;
+                });
+        }
+    }]);
