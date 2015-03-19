3a8beadfe3b0d81fd26a95e5c3c0bfc694257af5
Søren <S_englund0110@hotmail.com>
2015-03-19 18:06:31 +0100

    Happiness factor is now working
    Søren og Frederik

--- public/app/view4/view4.js
+++ public/app/view4/view4.js
@@ -10,24 +10,18 @@ angular.module('myAppRename.view4', ['ngRoute'])
     }])
 
     .controller('View4Ctrl', ['$scope', '$http', '$timeout', function ($scope, $http, $timeout) {
+        var alength = 3;
+        var blength = 3;
+        var list = [];
         $http({
             method: 'GET',
-            url: 'http://localhost:9292/firstRound/priority'
+            url: 'http://localhost:9292/firstRound/subject'
+
+        }).success(function (data) {
+            console.log("JSON: " + angular.toJson(data));
+            $scope.persons = data;
 
         }).
-            success(function (data) {
-                var map = {};
-                for(var i = 0; i < data.length; i++){
-                    map = map[data[i].title] = "1";
-                    console.log("Map: " + map)
-                }
-
-                console.log("Data: " + map)
-
-                $scope.result = data[6].priority
-                $scope.subjects = data[0].title
-
-            }).
             error(function (status) {
                 var showInfo = true;
                 if (status != 200) {
@@ -36,4 +30,123 @@ angular.module('myAppRename.view4', ['ngRoute'])
                     $scope.message = "Could not get any data. Please try reload the page!";
                 }
             });
+        $scope.open = function (item) {
+
+            if ($scope.isOpen(item)) {
+                $scope.opened = undefined;
+            } else {
+                $scope.opened = item;
+            }
+        };
+        $scope.isOpen = function (item) {
+            return $scope.opened === item;
+        };
+        $scope.anyItemOpen = function () {
+            return $scope.opened !== undefined;
+        };
+        $scope.addToFirstPriority = function (title, description, teacher, pri, id) {
+            var json = {"priority": pri, "studentId" : id, "title": title, "description": description, "teacher": teacher}
+
+
+            if (alength > 2) {
+                $scope.aaPriority = json;
+                list.push(json)
+                alength = 1
+            }
+            else {
+                if (alength < 2) {
+                    $scope.abPriority = json;
+                    alength = 0;
+                    list.push(json)
+                }
+            }
+            if (alength == 0) {
+                $scope.buttonStateFirst = "btn-lg disabled"
+            }
+        }
+        $scope.addToSecondPriority = function (title, description, teacher, pri, id) {
+            var json = {"priority": pri, "studentId" : id, "title": title, "description": description, "teacher": teacher}
+            if (blength > 2) {
+                $scope.baPriority = json;
+                blength = 1
+                list.push(json)
+            }
+            else {
+                if (blength < 2) {
+                    $scope.bbPriority = json;
+                    blength = 0;
+                    list.push(json)
+                }
+            }
+
+            if (blength == 0) {
+                $scope.buttonStateSecond = "btn-lg disabled"
+            }
+
+        }
+        $scope.removeaa = function (element) {
+            var remove = list.indexOf(element)
+            if (remove > -1) {
+                list.splice(remove, 1)
+                element;
+                $scope.aaPriority = "";
+                alength = 3;
+                $scope.buttonStateFirst = "btn-lg enabled"
+            }
+            console.log(angular.toJson(list));
+        };
+        $scope.removeab = function (element) {
+            var remove = list.indexOf(element)
+            if (remove > -1) {
+                list.splice(remove, 1)
+                element;
+                $scope.abPriority = "";
+                alength = 1;
+                $scope.buttonStateFirst = "btn-lg enabled"
+            }
+            console.log(angular.toJson(list));
+        };
+        $scope.removeba = function (element) {
+            var remove = list.indexOf(element)
+            if (remove > -1) {
+                list.splice(remove, 1)
+                element;
+                $scope.baPriority = "";
+                blength = 3;
+                $scope.buttonStateSecond = "btn-lg enabled"
+            }
+            console.log(angular.toJson(list));
+        };
+        $scope.removebb = function (element) {
+            var remove = list.indexOf(element)
+            if (remove > -1) {
+                list.splice(remove, 1)
+                element;
+                $scope.bbPriority = "";
+                blength = 1;
+                $scope.buttonStateSecond = "btn-lg enabled"
+            }
+            console.log(angular.toJson(list));
+        };
+        $scope.getHappiness = function () {
+
+            var a1 = list[0].title;
+            var a2 = list[1].title;
+            var a3 = list[2].title;
+            var a4 = list[3].title;
+
+            var json = {"poolA": [a1, a2],"poolB": [a1, a2],
+                "studentID": ["Bjarke Carlsen","Martin Olgenkjær","Henrik Stavnem","Nicklas Thomsen"]}
+
+            console.log(json);
+            $http
+                .post("http://localhost:9292/firstRound/happiness", json)
+                .success(function (data, status) {
+
+                    $scope.result = data;
+                })
+                .error(function (data, status, error) {
+                    $scope.error = data;
+                });
+        }
     }]);
